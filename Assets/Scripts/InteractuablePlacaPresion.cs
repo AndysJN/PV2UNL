87d46ec9ebf6843be63af33964de9437b2c141e9
AndysJN <andrewsjn@gmail.com>
2025-09-05 08:56:21 +0200

    Implementada Placa de Presion
    Se agrega un Script InteractuablePlacaPresion

--- Assets/Scripts/InteractuablePlacaPresion.cs
+++ Assets/Scripts/InteractuablePlacaPresion.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractuablePlacaPresion : MonoBehaviour
 {
-    
+    public UnityEvent OnPlacaTriggered;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OnPlacaTriggered.Invoke();
+            Debug.Log("El objeto Jugador: " + other.gameObject.name + " interactuo con la PLACA: " 
+                      + gameObject.name);
+        }
+    }
 }
