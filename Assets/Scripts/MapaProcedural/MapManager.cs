c4acf8ff1a0f9840af85392694598dcf8c3e7ba5
AndysJN <andrewsjn@gmail.com>
2025-10-03 14:06:33 +0200

    Cambios Menores al map manager.

--- Assets/Scripts/MapaProcedural/MapManager.cs
+++ Assets/Scripts/MapaProcedural/MapManager.cs
@@ -4,6 +4,12 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Spawneable;
+    }
+
+    private CellData[,] BoardData;
     private Tilemap TileMapGround;
     private Tilemap TileMapBorder;
 
@@ -19,23 +25,30 @@ public class BoardManager : MonoBehaviour
             .FirstOrDefault(tm => tm.name == "Ground");
         TileMapBorder = GetComponentsInChildren<Tilemap>(true)
             .FirstOrDefault(tm => tm.name == "CantWalk");
+        
+        BoardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; ++y)
         {
             for(int x = 0; x < Width; ++x)
             {
-                Tile GroundTile = null;
                 Tile BorderTile = null;
+                BoardData[x,y] = new CellData();
               
                 if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
                     BorderTile = BorderTiles[Random.Range(0, BorderTiles.Length)];
+                    BoardData[x,y].Spawneable = false;
                 }
+                // else
+                // {
+                //     BoardData[x,y].Spawneable = true; //A futuro puedo definir aca donde spawnearan los comestibles / enemigos. No Quiero que spawneen en los bordes de la pantalla...
+                // }
 
-                    GroundTile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                Tile GroundTile = GroundTiles[Random.Range(0, GroundTiles.Length)];
 
               
-                if (GroundTile != null) TileMapGround.SetTile(new Vector3Int(x, y, 0), GroundTile);
+                TileMapGround.SetTile(new Vector3Int(x, y, 0), GroundTile);
                 if (BorderTile != null) TileMapBorder.SetTile(new Vector3Int(x, y, 0), BorderTile);
             }
         }
