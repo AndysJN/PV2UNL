5dd6fd243660e3f6bef68222ab7610fe53587e3b
AndysJN <andrewsjn@gmail.com>
2025-10-26 23:36:36 +0100

    Refactor Meta class to default to NextScene in case no listener is linked to event.

--- Assets/Scripts/Meta.cs
+++ Assets/Scripts/Meta.cs
@@ -9,7 +9,31 @@ public class Meta : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-           OnCharacterMapReached.Invoke(other.gameObject.GetComponent<CharacterBase>().IsAlive(), other.gameObject.GetComponent<Puntaje>().GetScore());
+            if (HasValidPersistentListeners(OnCharacterMapReached))
+            { 
+                OnCharacterMapReached.Invoke(other.gameObject.GetComponent<CharacterBase>().IsAlive(), other.gameObject.GetComponent<Puntaje>().GetScore());
+            }
+            else
+            {
+                if (GameManager.Instance != null)
+                    GameManager.Instance.GotoNextScene();
+                else
+                    Debug.LogWarning("[Meta] No hay listeners y GameManager.Instance es null; no se puede cambiar de escena.", this);
+            }
         }
+    }
+    
+    private bool HasValidPersistentListeners(UnityEvent<bool, int> evt)
+    {
+        if (evt == null) return false;
+        int count = evt.GetPersistentEventCount();
+        for (int i = 0; i < count; i++)
+        {
+            var target = evt.GetPersistentTarget(i);
+            var method = evt.GetPersistentMethodName(i);
+            if (target != null && !string.IsNullOrEmpty(method))
+                return true;
+        }
+        return false;
     }
 }
